9fd83b658628e08ae45412076eb67778d5b338a3
Ilia <120221293+striction10@users.noreply.github.com>
2025-06-06 02:48:20 +0700

    last Admin

---
+++ src/views/techmarket/src/pages/Admin.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Home.css';
+import Admininfo from "../components/admininfo";
+
+const Admin = () => {
+    return(
+        <div className="Main">
+            <div className="mainBlock">
+                <Admininfo />
+            </div>
+            <div className="sideBar">
+            </div>
+        </div>
+    );
+}
+
+export default Admin;
